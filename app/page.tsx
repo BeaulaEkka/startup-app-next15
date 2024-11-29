2571aa1b335461dd1da5c14e12783080e08a75b7
Your Name <ekka.beaula@gmail.com>
2024-11-29 23:13:14 +0100

    servercomponent HMRcache

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+import React from "react";
 
-export default function Home() {
-  console.log("what am I s=doing hrer?--server or client")
+export default async function page() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
+  if (!response.ok) throw new Error("unable to recieve response");
+  const albums = await response.json();
+
   return (
-    <div><h2>Home page</h2>
-      <p>test</p>
-        <p>test2</p>
+    <div>
+      {albums &&
+        albums.map((album: { id: string; title: string }) => (
+          <div key={album.id}>
+            <ol>
+              <li>{album.title}</li>
+            </ol>
+          </div>
+        ))}
     </div>
   );
 }

efd0c1299224c0ea580e32e7c7330d5c06b12479
Your Name <ekka.beaula@gmail.com>
2024-11-30 23:17:43 +0100

    books route

--- app/api/books/[id]/route.ts
+++ app/api/books/[id]/route.ts
@@ -6,6 +6,7 @@ export async function PUT(
 ) {
   const id = +context.params.id;
   const book = await request.json();
+  
   const index = books.findIndex((b) => b.id === id);
   books[index] = books;
   return Response.json(books);

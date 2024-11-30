fbd0feb70f84cf5bba4acea46ee629abd20fe544
Your Name <ekka.beaula@gmail.com>
2024-11-30 23:15:48 +0100

    create new api route for backend

---
+++ app/api/db.ts
@@ -0,0 +1,8 @@
+const books = [
+  { id: 1, name: "Atomic Habits" },
+  { id: 2, name: "The 7 Habits" },
+  { id: 3, name: "The 4 Hour Work Week" },
+  { id: 4, name: "The 10X Rule" },
+  { id: 5, name: "The 5 AM Club" },
+];
+export default books;

459b0ea7fc79fab511b1144699f46b296f86039e
Your Name <ekka.beaula@gmail.com>
2024-12-02 23:46:04 +0100

    authentication google

--- auth.ts
+++ auth.ts
@@ -1,12 +1,9 @@
-// import NextAuth from "next-auth"
- 
-// export const { handlers, signIn, signOut, auth } = NextAuth({
-//   providers: [],
-// })
 
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
- 
+
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google"
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [GitHub],
-})
+  providers: [GitHub, Google],
+});

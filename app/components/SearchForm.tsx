367f9ea107cf1370f4d134c3e0126969fb93bab8
Your Name <ekka.beaula@gmail.com>
2024-12-03 22:37:32 +0100

    searchformreset

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import SearchFormReset from "./SearchFormReset";
 
 const SearchForm = () => {
+  const query = "Test";
+
   return (
     <div className="text-center rounded-full">
-      <input type="text" className='rounded-full px-2 py-2 border-2 border-black w-[50%] ' />
+      <form action="/" scroll={false} className="">
+        {" "}
+        <input
+          type="text"
+          placeholder="Search Startups"
+          defaultValue=""
+          name="query"
+          className="text-black rounded-full px-2 py-2 border-2 border-black w-[50%] "
+        />
+        <div className="flex gap-2">{query && <SearchFormReset />}</div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default SearchForm
+export default SearchForm;
---
+++ app/components/SearchFormReset.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+const SearchFormReset = () => {
+    
+  const reset = () => {
+    const form = document.querySelector(".search-form") as HTMLFormElement;
+    if (form) form.reset();
+  };
+  return (
+    <div>
+      <button type="reset" onClick={reset}>
+        reset
+      </button>
+    </div>
+  );
+};
+
+export default SearchFormReset;

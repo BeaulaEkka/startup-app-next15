847c5d8433af021d9647bdbb660f36f1db343bb0
Your Name <ekka.beaula@gmail.com>
2024-12-03 14:28:03 +0100

    layer utilities globalcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,9 @@
 import React from "react";
 
 export default async function page() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
-  if (!response.ok) throw new Error("unable to recieve response");
-  const albums = await response.json();
-
   return (
-    <div className="flex flex-col w-[80%] mx-auto mt-20">
-      <h1 className="font-bold text-2xl mb-2 underline-offset-2">Movies</h1>
-      {albums &&
-        albums.map((album: { id: string; title: string }) => (
-          <div key={album.id}>
-            <ul className="capitalize flex  mt-2 ">
-              <li className="capitalize">
-                {album.id}. {album.title}
-              </li>
-            </ul>
-          </div>
-        ))}
+    <div>
+      <h1 className="heading">Home</h1>
     </div>
   );
 }

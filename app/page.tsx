2e96f7e1bdf010abd7af22229b202fc2b9c408aa
Your Name <ekka.beaula@gmail.com>
2024-12-03 20:51:45 +0100

    hero -searchcomponent

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export default async function page() {
   return (
-    <div>
-      <h1 className="heading">Home</h1>
-    </div>
+    <section className="pink_container">
+      <h1 className="heading">
+        pitch your startups, connect with entrepreneurs
+      </h1>
+      <p className="text-center text-xl capitalize mt-8">Submit Ideas,Vote on pitches and get Noticed in Virtual Competitons</p>
+
+    </section>
   );
 }

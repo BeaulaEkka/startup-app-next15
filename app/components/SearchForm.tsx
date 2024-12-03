ce7db5d52edd1f33e321bd5e4e20e94ca5800e2c
Your Name <ekka.beaula@gmail.com>
2024-12-03 21:08:10 +0100

    searchForm-start

---
+++ app/components/SearchForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchForm = () => {
+  return (
+    <div className="text-center rounded-full">
+      <input type="text" className='rounded-full px-2 py-2 border-2 border-black w-[50%] ' />
+    </div>
+  )
+}
+
+export default SearchForm
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SearchForm from "./components/SearchForm";
 
 export default async function page() {
   return (
@@ -6,8 +7,10 @@ export default async function page() {
       <h1 className="heading">
         pitch your startups, connect with entrepreneurs
       </h1>
-      <p className="text-center text-xl capitalize mt-8">Submit Ideas,Vote on pitches and get Noticed in Virtual Competitons</p>
-
+      <p className="text-center text-xl capitalize mt-8 mb-8">
+        Submit Ideas,Vote on pitches and get Noticed in Virtual Competitons
+      </p>
+      <SearchForm />
     </section>
   );
 }

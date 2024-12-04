07327a7b9b694fa1db73901cca9f28f599f159fc
Your Name <ekka.beaula@gmail.com>
2024-12-04 23:47:20 +0100

    hero -complete

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,19 @@ export default async function page({
 }) {
   const query = (await searchParams).query;
   return (
-    <section className="pink_container">
-      <h1 className="heading">
-        pitch your startups, connect with entrepreneurs
-      </h1>
-      <p className="text-center text-xl capitalize mt-8 mb-8">
-        Submit Ideas,Vote on pitches and get Noticed in Virtual Competitons
-      </p>
-      <SearchForm query={query} />
-    </section>
+    <div>
+      <section className="pink_container">
+        <h1 className="heading">
+          pitch your startups, connect with entrepreneurs
+        </h1>
+        <p className="text-center text-xl capitalize mt-8 mb-8">
+          Submit Ideas,Vote on pitches and get Noticed in Virtual Competitons
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="capitalize ">{query? `search results for "${query}"` : "All startups"}</p>
+      </section>
+    </div>
   );
 }

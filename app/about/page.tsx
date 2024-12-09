043beb87b6fc945098cd5285a0ff112b5ba3ec0b
Your Name <ekka.beaula@gmail.com>
2024-12-09 23:54:28 +0100

    removed error about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 export default function page() {
-  throw new Error("not implemented");
+  // throw new Error("not implemented");
   return <div>This is about page</div>;
 }
--- app/startup/[id]/page.tsx
+++ app/startup/[id]/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const id = (await params).id;
+  return <div className="w-[80%] mx-auto mt-10">page {id}</div>;
 };
 
 export default page;

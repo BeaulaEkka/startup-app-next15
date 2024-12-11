b2e9fa0f21d0f6c7ad64583482642494823c47f4
Your Name <ekka.beaula@gmail.com>
2024-12-11 23:49:03 +0100

    ping updated

--- app/components/View.tsx
+++ app/components/View.tsx
@@ -5,7 +5,7 @@ const View = ({ id }: { id: string }) => {
   return (
     <div className="view-tag">
       View
-      <div className="absolute -top-2 -right-2">
+      <div className="absolute -top-2 right-1">
         <Ping />
       </div>
     </div>

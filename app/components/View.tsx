79aaaff8854fcc2eb426a5f0a3ea780fc553ee4f
Your Name <ekka.beaula@gmail.com>
2024-12-13 21:40:34 +0100

    view component

--- app/components/View.tsx
+++ app/components/View.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import Ping from "./Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
   console.log(totalViews);
+
+  await writeClient
+    .patch(id)
+    .set({ view: totalViews + 1 })
+    .commit();
+
   return (
     <div className="view-tag">
       <p>views:{totalViews}</p>

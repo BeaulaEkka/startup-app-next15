44280d9723a27041984ecb413010117c1823c2f2
Your Name <ekka.beaula@gmail.com>
2024-12-13 22:21:31 +0100

    after

--- app/components/View.tsx
+++ app/components/View.tsx
@@ -3,6 +3,7 @@ import Ping from "./Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import { writeClient } from "@/sanity/lib/write-client";
+import { after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
@@ -10,10 +11,12 @@ const View = async ({ id }: { id: string }) => {
     .fetch(STARTUP_VIEWS_QUERY, { id });
   console.log(totalViews);
 
-  await writeClient
-    .patch(id)
-    .set({ view: totalViews + 1 })
-    .commit();
+  after(async () => {
+    await writeClient
+      .patch(id)
+      .set({ view: totalViews + 1 })
+      .commit();
+  });
 
   return (
     <div className="view-tag">
